df173f302eb2d10aaf85995cf592c1eb1fea6b26
Gurinder Singh <gurinder.qudos@gmail.com>
2021-05-15 18:08:51 +0530

    Initial Commit

---
+++ A1_iOS_GurinderSingh_C0806087/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A1_iOS_GurinderSingh_C0806087
+//
+//  Created by Gurinder Singh on 15/05/21.
+//  Copyright © 2021 Gurinder Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
